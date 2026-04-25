0f7509fbf9ef53075002d1adc44ff375133b4eb6
Avni Shrivastava <avnishrivastava1407@gmail.com>
2026-04-25 23:02:31 +0530

    Modified whole blueprint

---
+++ routes/tasks_routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from db import make_connection
+from auth import verify_token
+
+router=APIRouter()
+
+@router.get("/all")
+def get_all_tasks (current_user: str= Depends(verify_token)):
+    connect_db = make_connection()
+    cursor = connect_db.cursor()
+
+    cursor.execut('''SELECT * FROM tasks''')
+    tasks=cursor.fetchall()
+    connect_db.close()
+
+    return {"tasks":[dict(task) for task in tasks] }
+
+
